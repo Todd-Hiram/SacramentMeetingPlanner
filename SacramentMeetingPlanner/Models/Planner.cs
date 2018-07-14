6cad196b7d3a024505cc15484f56cf8a9f1393df
nbunch2 <n.bunch5683@gmail.com>
2018-07-14 09:45:08 -0600

    07/14 Data Models Created

---
+++ SacramentMeetingPlanner/Models/Planner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Planner
+    {
+        public int ID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime MeetingDate { get; set; }
+        [Display(Name = "Conducting")]
+        public string BishopricMemeber { get; set; }
+        [Display(Name = "Opening Hymn")]
+        public string OpeningHymn { get; set; }
+        [Display(Name = "Sacrament Hymn")]
+        public string SacramentHymn { get; set; }
+        [Display(Name = "Intermediate Hymn")]
+        public string IntermediateHymn { get; set; }
+        [Display(Name = "Closing Hymn")]
+        public string ClosingHymn { get; set; }
+
+        public IEnumerable<Speakers> Speakers { get; set; }
+    }
+}
---
+++ SacramentMeetingPlanner/Models/Speakers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Speakers
+    {
+        public int SpeakerID { get; set; }
+       public int PlannerID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Topic { get; set; }
+
+
+       public Planner Planner { get; set; }
+    }
+}
